0d9ebf4957a236d92c2c758cf0e162bc77a00292
Abed Fayyad <abed.ef@icloud.com>
2023-05-28 14:15:27 -0400

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,481 @@
+<script lang="ts">
+    let courses = [
+        { code: "CSC209", name: "Software Tools and Systems Programming" },
+        { code: "CSC236", name: "Introduction to the Theory of Computation" },
+        { code: "CSC258", name: "Computer Organization" },
+        { code: "CSC263", name: "Data Structures and Analysis" },
+        { code: "CSC300", name: "Computers and Society" },
+        { code: "CSC301", name: "Introduction to Software Engineering" },
+        { code: "CSC302", name: "Engineering Large Software Systems" },
+        { code: "CSC309", name: "Programming on the Web" },
+        { code: "CSC318", name: "Design of Interactive Computational Media" },
+        { code: "CSC324", name: "Programming Languages" },
+        { code: "CSC343", name: "Introduction to Databases" },
+        { code: "CSC369", name: "Operating Systems" },
+        { code: "CSC373", name: "Algorithm Design, Analysis and Complexity" },
+        { code: "CSC384", name: "Introduction to Artificial Intelligence" },
+        { code: "CSC401", name: "Natural Language Computing" },
+        { code: "CSC404", name: "Video Game Design" },
+        { code: "CSC418", name: "Computer Graphics (Audited)" },
+        { code: "CSC458", name: "Computer Networking Systems" },
+        { code: "CSC469", name: "Advanced Operating Systems (Audited)" },
+        { code: "CSC488", name: "Compilers and Interpreters" },
+    ];
+</script>
+
+<!-- TODO: Make this page viewable in mobile -->
+<!-- TODO: Hide contents when page is too narrow -->
+
+<svelte:head>
+    <title>Resume | Abed Fayyad</title>
+</svelte:head>
+
+<header>
+    <div id="header-left">
+        <div>
+            <a href="https://gitlab.com/abed">gitlab.com/abed</a>
+        </div>
+        <div>
+            <a href="https://github.com/abedef">github.com/abedef</a>
+        </div>
+    </div>
+
+    <div id="header-center">
+        <h1>Abed Fayyad</h1>
+    </div>
+
+    <div id="header-right">
+        <div>
+            <a href="mailto:Abed <hello@abedef.ca>">hello@abedef.ca</a>
+        </div>
+        <div>
+            <a href="tel:+1 (416) 452-1467">+1 (416) 452-1467</a>
+        </div>
+    </div>
+</header>
+
+<section>
+    <h3>Summary</h3>
+    <div>
+        <p>
+            Cloud software & infrastructure specialist with 5+ years of
+            experience developing, deploying and maintaining large microservice
+            systems in telecommunications and telehealth.
+        </p>
+    </div>
+</section>
+
+<!-- TODO: Include "I am comfortable working with XYZ languages..." -->
+
+<section>
+    <h3>Experience</h3>
+    <div>
+        <div class="company">
+            <h3>Postscriptions Inc.</h3>
+            <h3>2021 – Present</h3>
+        </div>
+        <p class="company-description">
+            Since my last semester of university, I spent my evenings and
+            weekends designing and prototyping an online pharmacy solution with
+            a friend from high school. While he managed the pharmacy and
+            regulation side of things, I handled our technical requirements and
+            built up a stack to fulfil our patients' needs. In September of
+            2021, we were well-off enough that I could afford to go full-time.
+        </p>
+        <p>
+            This was a significant undertaking and one of my most difficult
+            experiences, but I am glad I did it nonetheless. The project forced
+            me to pick up many new skills and finally face app- and web-
+            development, which I had been neglecting so far. Throughout my time
+            working on the project, I had the opportunity to move between
+            technical stacks until I found the right fit.
+        </p>
+
+        <div class="role">
+            <h4>Founding Developer</h4>
+            <em>Full Stack Development</em>
+        </div>
+
+        <ul class="responsibilities">
+            <li>Online delivery pharmacy built with SvelteKit</li>
+            <li>Recently migrated from a Go API with a native iOS client</li>
+            <li>
+                With over two years of experience building a custom online
+                pharmacy solution, I have encountered many obstacles that come
+                with this undertaking. Due to the nature of the data we work
+                with, we are forced to rely on PIPEDA-compliant tools, or tools
+                with self-hosted distributions that I can deploy and support
+                locally. Additionally, we implemented password-less login via a
+                variety of methods including magic links, one-time-code
+                verification, OAuth and WebAuthn. Pharmacists communicate with
+                patients through our custom portal, interfacing with our
+                internal chat platform, and Twilio’s SMS and Email services for
+                notification purposes.
+            </li>
+            <li>
+                due to the nature of the data we were working with, we relied
+                mainly on tools with self-hosted versions and I supported those
+                services
+            </li>
+
+            <li>
+                passwordless login (magic link, code verification, oauth,
+                webauthn)
+            </li>
+            <li>notifications (Twilio and SendGrid)</li>
+            <li>SMS messaging integration with Mattermost</li>
+            <li>
+                Patients can view and manage their prescriptions or coordinate
+                deliveries
+            </li>
+            <li>SvelteKit, Go, Swift/SwiftUI, PostgreSQL, GCP</li>
+            <li>
+                At this stage our application is supported by a Go API with a
+                native iOS client, and web app written in Svelte. Patients can
+                view and manage their prescriptions, and coordinate deliveries.
+                Features passwordless login via SMS or email, implemented from
+                scratch with Twilio's APIs.
+            </li>
+            <li>
+                co-founded an online pharmacy and took ownership of the
+                technical side of the business, interfacing with designers and
+                other engineers to accomplish our goals in a fast-paced startup
+                environment with a focus on keeping GCP operating costs to a
+                minimum
+            </li>
+            <li>
+                Technical requirements to build up a stack to fulfil our
+                patients' needs
+            </li>
+            <li>
+                Created prototype API in Java Spring and eventually transitioned
+                to Go to make use of fast build and deploy times, and supports a
+                tier on GCP that makes things cheap for us
+            </li>
+            <li>Built front end app in iOS SwiftUI</li>
+            <li>Built back end management app for pharmacists in Svelte</li>
+            <li>Migrated to full SvelteKit backend, lighter and cheaper</li>
+        </ul>
+
+        <div class="company">
+            <h3>Bell Canada</h3>
+            <h3>2017 – 2021</h3>
+        </div>
+        <p class="company-description">
+            Straight out of school I joined a small and fairly new Agile team at
+            Bell that was due to deliver on an ambitious project with a tight,
+            government-mandated deadline. The CRTC updated their wireless code
+            of conduct – which all telecommunications companies must follow –
+            and Bell was the first to implement their requirements without
+            delays thanks to the work done by my team. Later, I transferred to a
+            team responsible for the rollout of 5G onto Bell's network, where I
+            supported our Kubernetes environments. <!-- Something about replacing legacy systems with microservice architecture -->
+            <!-- collaborated with people all the way from Dublin to Delhi -->
+        </p>
+
+        <div class="role">
+            <h4>Software Developer</h4>
+            <em>Infrastructure & Pipeline Automation</em>
+        </div>
+
+        <ul class="responsibilities">
+            <li>
+                supported and developed systems supporting high TPS and high
+                redundancy for charging systems at a nationwide
+                telecommunication company
+            </li>
+            <li>
+                developed and supported high TPS systems with highly redundant
+                deployments at a nationwide telecom company
+            </li>
+            <li>
+                tracked down opportunities to improve and speed up development
+                and deployment pipelines
+            </li>
+            <li>
+                tracked down opportunities to improve and speed up our
+                development and deployment pipelines, with an emphasis on
+                enhancing developer and QA experience
+            </li>
+            <li>
+                advocated for knowledge sharing and reduced redundancy by means
+                of organization-wide “open-source” development practices
+            </li>
+            <li>
+                troubleshooted development and production environment issues,
+                providing technical support where required, and documenting
+                fixes and best practices in our team’s shared knowledge base
+            </li>
+            <li>
+                troubleshooted development and production environment issues,
+                documenting fixes and best practices in our shared knowledge
+                base
+            </li>
+            <li>
+                introduced, trialed and rolled out several eventual key
+                infrastructure and development tools (i.e. Terraform)
+            </li>
+
+            <li>
+                Know design patterns, implemented a bunch of features with
+                minimal guidance
+            </li>
+            <li>
+                Wrote a lot of reconciliation scripts that are probably still
+                there today
+            </li>
+            <li>
+                Supported application deployments to Openshift and their
+                integration
+            </li>
+            <li>
+                Developed pipelines to accelerate deployments and minimize human
+                error
+            </li>
+            <li>
+                Presented knowledge-sharing talks and demos on automation and
+                Linux tools
+            </li>
+        </ul>
+
+        <div class="role">
+            <h4>DevOps Engineer</h4>
+            <em>DevOps and Java Spring Development</em>
+        </div>
+
+        <ul class="responsibilities">
+            <li>Built and configured first development Docker swarm</li>
+            <li>
+                Built multiple QA and production environments (Ansible,
+                Terraform)
+            </li>
+            <li>
+                I introduced (or was instrumental in the adoption of) a number
+                of tools to the team's stack, from which I can proudly say
+                several still remain foundational to the team's infrastructure
+                (i.e. Terraform, Portainer, GitLab CI).
+            </li>
+            <li>
+                Docker and Kubernetes (OpenShift) Ansible Terraform Jenkins,
+                later GitLab CI Vagrant OpenStack Zabbix Graylog
+            </li>
+            <li>
+                Took charge in setting up automated development environment
+                deployments
+            </li>
+            <li>
+                Wrote and took ownership of deployment scripts for setting up
+                development and production environments
+            </li>
+            <li>Maintained scripts for production environment management</li>
+            <li>
+                Worked with DBAs to automate geo-redundant MongoDB deployments
+            </li>
+            <li>
+                As well during my time at Bell I wrote a script which was a key
+                project deliverable. It provided a Terraform template and
+                Ansible playbook to deploy a geo-redundant multi-node cluster
+                that would support enormous streams of data. Later, I
+                collaborated with our RDBMS specialists and Mongo's on-site
+                support team to optimize the deployments. This consisted of
+                dozens of test deployments, experimenting with a number of our
+                server's OS configurations (THP, security policies, hypervisor
+                considerations).
+            </li>
+            <li>
+                Developed features within an Agile-managed microservice
+                architecture
+            </li>
+        </ul>
+
+        <div class="company">
+            <h3>University of Toronto</h3>
+            <h3>2016 – 2017</h3>
+        </div>
+        <div class="role">
+            <h4>Teaching Assistant</h4>
+            <em>Introduction to Computer Science</em>
+        </div>
+    </div>
+</section>
+
+<section>
+    <h3>Skills</h3>
+    <div>
+        <ul>
+            <li>
+                Server administration: comfortable using Linux environment as a
+                daily driver
+            </li>
+            <li>
+                Home server administration (self-hosted services, build servers,
+                chat bots)
+            </li>
+            <li>boop SvelteKit, Go, Swift/SwiftUI, PostgreSQL, GCP</li>
+            <li>
+                Javascript, Typescript, Go, Swift, Python, HTML/CSS, Java, Ruby,
+                Rust
+            </li>
+            <li>Functional programming languages such as Racket and Elm</li>
+            <li>
+                Tools: Docker, Ansible, Packer, Vagrant, Terraform, Linux,
+                Openshift, Openstack, Google Cloud, HAProxy, Nginx, Traefik,
+                Jenkins, Gitlab CI/CD
+            </li>
+            <li>User interface design and development</li>
+            <li>
+                Creative coding, graphics programming and game engine
+                architecture
+            </li>
+            <li>
+                In a personal capacity, I enjoy coding and am usually hacking
+                away at a few side projects at any given time, which I
+                occasionally showcase on my Github/GitLab. I like to explore new
+                frameworks and languages and am always excited to add a new
+                language to my tool belt.
+            </li>
+            <li>
+                In terms of hobbies, since I was first taught to set up and use
+                SSH, I have been maintaining a number of home servers for the
+                purpose of supporting self-hosted services. Although my first
+                few servers' OS is Canonical's Ubuntu, I prefer Arch Linux for
+                personal use. Some of my favourite self-hosted tools are MinIO
+                and Proxmox, which I make generous use of in my personal
+                projects.
+            </li>
+            <li>
+                Know my way around the command line and have set up enough Linux
+                machines and Hackintoshes to feel comfortable
+            </li>
+            <li>Languages: English, Arabic, Russian</li>
+            <li><a href="/portfolio">Visual Portfolio</a></li>
+        </ul>
+    </div>
+</section>
+
+<section>
+    <h3>Education</h3>
+    <div>
+        <div class="company">
+            <h3>University of Toronto</h3>
+            <h3>2014 – 2019</h3>
+        </div>
+
+        <div class="role">
+            <h4>Honours Bachelor of Science (HBSc) with Distinction</h4>
+            <em>Computer Science Specialist Program</em>
+        </div>
+
+        <ul id="coursework">
+            {#each courses as course}
+                <li>{course.code} <em>{course.name}</em></li>
+            {/each}
+        </ul>
+    </div>
+</section>
+
+<section>
+    <h3>Open<br />Source<br />Contribution</h3>
+    <div>
+        <p>
+            In terms of larger code bases, my team at Bell made heavy use of
+            tools like Ansible, which happens to be written in Python. During
+            one of our runs, the tool misbehaved in a consistent way, so I filed
+            a <a href="https://github.com/ansible/ansible/issues/32906"
+                >bug report</a
+            >, and dove into the code to look for a
+            <a href="https://github.com/ansible/ansible/pull/32909"
+                >possible fix</a
+            >. Though my particular fix was not merged, my report was
+            acknowledged and a similar fix was employed.
+        </p>
+    </div>
+</section>
+
+<footer>
+    This resume is made ✨ from scratch ✨ using <a href="https://svelte.dev"
+        >Svelte</a
+    >!
+</footer>
+
+<style>
+    * {
+        font-family: Helvetica, sans-serif;
+    }
+
+    header {
+        display: flex;
+    }
+
+    header > * {
+        flex: 1;
+    }
+
+    section {
+        display: flex;
+        margin-top: 1rem;
+    }
+
+    section > h3 {
+        min-width: 8rem;
+        opacity: 0.25;
+    }
+
+    section > div {
+        flex: 1;
+    }
+
+    div.company {
+        display: flex;
+        justify-content: space-between;
+    }
+
+    div.role {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    footer {
+        text-align: center;
+    }
+
+    header a {
+        text-decoration: none;
+        color: currentColor;
+    }
+
+    p,
+    h1,
+    h2,
+    h3,
+    h4 {
+        margin: 0;
+    }
+
+    p.company-description {
+        margin-bottom: 1rem;
+    }
+
+    ul {
+        margin: 0;
+        padding-left: 1rem;
+    }
+
+    ul.responsibilities {
+        margin: 0;
+        margin-bottom: 1rem;
+    }
+
+    #header-center {
+        text-align: center;
+    }
+
+    #header-right {
+        text-align: right;
+    }
+
+    #coursework {
+        column-count: 2;
+    }
+</style>
---
+++ src/routes/portfolio/+page.svelte
@@ -0,0 +1,246 @@
+<svelte:head>
+	<title>Portfolio | Abed Fayyad</title>
+</svelte:head>
+
+<h1>Abed's Portfolio</h1>
+
+<p>
+	Most projects listed in my <a href="/">resume</a> are available on my
+	<a href="https://github.com/abedef/">GitHub</a>
+	or <a href="https://gitlab.com/abed/">GitLab</a>. For the handful of notable
+	projects that are private or are missing source code, I provide below all
+	the relevant material that I could scrounge up.
+</p>
+<ul>
+	<li>
+		<a href="#postscriptions">Postscriptions</a>: full stack solo project
+	</li>
+	<li><a href="#reddit-reader">Reddit Reader</a>: safari extension</li>
+	<li><a href="#gd-intwo">Game Development: InTwo</a></li>
+	<li><a href="#gd-hackathons">Game Development: Hackathons</a></li>
+	<li><a href="#open-source">Open Source Contributions</a></li>
+	<li>
+		<a href="#home-server">Home Server</a>: how I enhance my online life
+		with Linux
+	</li>
+</ul>
+
+<h2 id="postscriptions">Postscriptions</h2>
+
+<p>
+	This is a pharmacy I co-founded in my spare time, focused on providing a
+	digital pharmacy service complete with prescription delivery. I represent
+	the technical side of the company and have so far delivered a
+	cloud-optimized backend written in Go and hosted on Google's Cloud Platform
+	(GCP), along with a native iOS client written in Swift.
+</p>
+<p>
+	The app is built from scratch using Apple's SwiftUI framework, and features
+	a number of custom-built user interface components <sub
+		>to make up for SwiftUI's lack of featured components ☹️</sub
+	>. This was so far my greatest challenge in user interface design and
+	development and I am quite pleased with the result, especially the landing
+	page and login flow (a demo follows).
+</p>
+<p>
+	Another interesting technical challenge was implementing secure user
+	authentication. To minimize the possibility of malicious logins and
+	forgotten passwords, we opted for an OTC-based login solution, where codes
+	are texted or emailed to users using Twilio's SMS and email gateways.
+</p>
+<p>
+	Our next milestone is the web client, which I am currently developing. My
+	experience in the web stack has so far been limited to university projects
+	and hobby projects, so working on a production-level web app has been a
+	learning experience.
+</p>
+<p>
+	Here is our <a href="https://postscriptions.ca">webpage</a> and here is a
+	link to our
+	<a href="https://apps.apple.com/ca/app/postscriptions/id1504547619"
+		>app store page</a
+	>.
+</p>
+<video controls="controls" width="100%" height="100%" src="posty.mov" />
+
+<h2 id="reddit-reader">Reddit Reader</h2>
+
+<video controls="controls" width="100%" height="100%" src="reddit-reader.mov" />
+
+<p>
+	During the days of the reddit layout of yore, I yearned for vim-like
+	navigation with
+	<code>J</code> and <code>K</code> to move between and collapse comments. This
+	was not a very complicated project but it was my first Safari extension which
+	I finished.
+</p>
+<p>
+	The small amount of code I used to implement this is viewable
+	<a href="https://github.com/abedef/redditreader">on GitHub</a>. Of course,
+	after all my hard work, Reddit changed its layout and my extension was no
+	longer necessary, as they had themselves implemented keyboard navigation. By
+	this point, I had moved on to:
+</p>
+
+<h2 id="gd-intwo">Game Development: InTwo</h2>
+<iframe
+	width="100%"
+	src="https://www.youtube.com/embed/bsmkVOADfEM"
+	frameborder="0"
+	allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope;
+				picture-in-picture"
+	allowfullscreen
+/>
+<p style="margin-top: 5%">
+	I definitely could have done a better job keeping track of my old projects
+	but fortunately I was able to track down my teammate's <a
+		href="https://feyla.ca/?portfolio=intwo-a-game-of-hexagons"
+	>
+		portfolio page</a
+	>
+	which showcases some screenshots and a trailer for
+	<em>InTwo</em>, the game we created for our university game development
+	course.
+</p>
+<p>
+	InTwo started as an atmospheric audio-based puzzle game, but technical
+	challenges and deadlines forced us to pivot and redesign the game as a couch
+	coop puzzle game.
+</p>
+<p>
+	Our class's projects were showcased at the <a
+		href="https://www.facebook.com/events/1864898127087351/"
+	>
+		7th Annual Level Up Showcase</a
+	>, and InTwo was used in one of the
+	<a
+		href="https://www.facebook.com/UofTCSSU/videos/come-level-up-csc404-video-game-design-students-present-their-work-to-the-public/1298622390224588/"
+	>
+		CSSU's teasers</a
+	> for the event.
+</p>
+<p>
+	Thanks to iCloud, I <em>was</em> able to fish out some WIP materials and some
+	of my Blender screen captures which I excitedly shared with friends in the past.
+</p>
+
+<video controls="controls" width="100%" src="intwo-shader-effect.mov" />
+<sub
+	>Prototyping shader effects, which were meant to accompany players' shout
+	animations.</sub
+>
+
+<img src="intwo-firegirl.png" width="100%" />
+<video controls="controls" width="100%" src="intwo-waves.mov" />
+<video controls="controls" width="100%" src="intwo-obelisk.mov" />
+
+<video controls="controls" width="100%" src="intwo-ghost-butt.mp4" />
+
+<video controls="controls" width="100%" src="intwo-ghost-walk.mp4" />
+
+<video controls="controls" width="100%" src="intwo-run.mp4" />
+<sub>A showcase of the game's characters' idle, run and shout animations.</sub>
+
+<video
+	controls="controls"
+	muted="true"
+	width="100%"
+	src="intwo-starfield.mp4"
+/>
+<sub>Using Blender's scripting capabilities to generate a starfield.</sub>
+
+<p>
+	I was lucky to work with a talented joint team composed of OCAD students and
+	students from UofT's computer science program. However, due to shortages in
+	enrolment, we had plenty of programmers and even a sound engineer, but no
+	modellers or animators! This project forced me to dip my toes into character
+	modelling and animation, and gave me my first experience writing shaders.
+</p>
+
+<h2 id="gd-hackathons">Game Development: Hackathons</h2>
+
+<p>
+	My team took part in a number of hackathons throughout our course, where we
+	practiced using Unity to prototype our games. At the time, I was captivated
+	by voxel art and created simple models for our games using
+	<a href="https://www.voxelmade.com/magicavoxel/">Magicavoxel</a>.
+</p>
+
+<img src="hackathon-pizza.png" width="100%" />
+<img src="hackathon-steve.png" width="100%" />
+<video controls="controls" width="100%" src="hackathon-voxels.mov" />
+
+<img src="hackathon-granny.png" width="100%" />
+<img src="hackathon-hood.png" width="100%" />
+
+<h2 id="open-source">Open Source Contributions</h2>
+
+<h6>Ansible</h6>
+<p>
+	While using Ansible during my coop I noticed a bug and opened <a
+		href="https://github.com/ansible/ansible/issues/32906">an issue</a
+	>. Since it is written in Python, I was able to pinpoint the erroneous code
+	and submit a fix that handled my use-case. Although my
+	<a href="https://github.com/ansible/ansible/pull/32909">submitted fix</a> did
+	not cover all edge cases and was therefore not accepted, this was my first interaction
+	with the open source community and it left a positive impression on me.
+</p>
+
+<h6>Lockwise</h6>
+<p>
+	I later tracked down a user interface bug in Mozilla's iOS password manager
+	app, Lockwise, and <a
+		href="https://github.com/mozilla-lockwise/lockwise-ios/pull/1102"
+		>submitted a fix</a
+	>
+	which
+	<strong><em>was</em></strong> merged!
+</p>
+
+<h2 id="home-server">Home Server</h2>
+
+<p>
+	Inspired by the Linux nerds in my university's computer science lounge, I
+	tried out many distros during my time as a student and eventually grew
+	comfortable with Arch Linux, which I currently run on my servers. Aside from
+	the usual use case of running dedicated game servers, I run...
+</p>
+
+<h6>CI/CD & Docker</h6>
+<p>
+	I set up GitLab runners to run pipelines for my projects that use GitLab CI.
+	Previously, I ran a Jenkins server for this purpose. Services are deployed
+	to my mildly geo-redundant two node Docker swarm. For example, my friends
+	and I maintain a bot for our Discord server which allows users to earn
+	points while logged in, and gamble or spend points in various pointless
+	ways. Whenever a change is pushed, the code is built into an image using a
+	GitLab runner deployed on the server, and redeployed.
+</p>
+<p>
+	To make it easier for users to upload their dog photos, I deployed <a
+		href="https://min.io">minio</a
+	>, an object store, as a Docker service. During some interesting Discord
+	events, we thought it would be neat to see a breakdown of user points, so I
+	built
+	<a href="https://abedef.ddns.net/gambling"> a rough dashboard</a> to show the
+	time-series data. I eventually hit some scaling issues impacting the page's load
+	time, which I have yet to resolve.
+</p>
+
+<h6>Media Server and WebDAV</h6>
+<p>
+	Since macOS allows you to natively attach a WebDAV volume via its Finder
+	application, I set up a secured WebDAV as a simple solution for DIY document
+	sharing. I also set up a RAID for my media server, which I maintain with the
+	help of scheduled scripts. Among other uses, these scripts allow me to
+	receive weekly speedtest and disk space reports to help keep tabs on things.
+</p>
+
+<h6>VPN Server</h6>
+<p>
+	To support my team's remote work needs I deployed strongSwan, an IPsec VPN
+	server. This allowed me to create an IKEv2 VPN which can <em>also</em> be natively
+	and simply configured on a macOS client. For good measure, I implemented a login
+	notification script which notifies me of each login's source IP and other details
+	via SMS, silencing notifications for logins from trusted networks.
+</p>
