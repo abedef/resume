8b62914321cb911715b3dd2915e9a80183010017
Abed Fayyad <abed.ef@icloud.com>
2023-05-29 19:48:29 -0400

    Various updates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -25,65 +25,55 @@
     let experience = [
         {
             company: "Postscriptions Inc.",
-            years: "2021 – Present",
+            years: "2021–Present",
             description:
-                "Since my last semester of university, I spent my evenings and weekends designing and prototyping an online pharmacy solution with a friend from high school. While he managed the pharmacy and regulation side of things, I handled our technical requirements and built up a stack to fulfil our patients' needs. In September of 2021, we were well-off enough that I could afford to go full-time. This was a significant undertaking and one of my most difficult experiences, but I am glad I did it nonetheless. The project forced me to pick up many new skills including web and app development.",
+                "In my last semester of university, I began designing and prototyping an online delivery pharmacy solution with a long-time friend. While he managed the pharmacy and regulatory side of business, I handled our technical requirements and built a stack to fulfill our patients' needs. By September of 2021 we had the vision, timeline and funding to allow me to pursue our venture full-time, with the end goal of leading our operational stack to a comfortable level of maturity and autonomy. At this point in time we operate out of a location in Toronto, offering prescription delivery all over the GTA.",
             roles: [
                 {
                     title: "Founding Engineer, Chief Technical Officer",
                     subtitle: "Full Stack Development",
                     responsibilities: [
-                        "Online delivery pharmacy built with SvelteKit",
-                        "Recently migrated from a Go API with a native iOS client",
-                        "With over two years of experience building a custom online pharmacy solution, I have encountered many obstacles that come with this undertaking. Due to the nature of the data we work with, we are forced to rely on PIPEDA-compliant tools, or tools with self-hosted distributions that I can deploy and support locally. Additionally, we implemented password-less login via a variety of methods including magic links, one-time-code verification, OAuth and WebAuthn. Pharmacists communicate with patients through our custom portal, interfacing with our internal chat platform, and Twilio’s SMS and Email services for notification purposes.",
-                        "due to the nature of the data we were working with, we relied mainly on tools with self-hosted versions and I supported those services",
-                        "passwordless login (magic link, code verification, oauth, webauthn)",
-                        "notifications (Twilio and SendGrid)",
-                        "SMS messaging integration with Mattermost",
-                        "Patients can view and manage their prescriptions or coordinate deliveries",
-                        "SvelteKit, Go, Swift/SwiftUI, PostgreSQL, GCP",
-                        "At this stage our application is supported by a Go API with a native iOS client, and web app written in Svelte. Patients can view and manage their prescriptions, and coordinate deliveries. Features passwordless login via SMS or email, implemented from scratch with Twilio's APIs.",
-                        "co-founded an online pharmacy and took ownership of the technical side of the business, interfacing with designers and other engineers to accomplish our goals in a fast-paced startup environment with a focus on keeping GCP operating costs to a minimum ",
-                        "Technical requirements to build up a stack to fulfil our patients' needs",
-                        "Created prototype API in Java Spring and eventually transitioned to Go to make use of fast build and deploy times, and supports a tier on GCP that makes things cheap for us",
-                        "Built front end app in iOS SwiftUI",
-                        "Built back end management app for pharmacists in Svelte",
-                        "Migrated to full SvelteKit backend, lighter and cheaper",
+                        "Took ownership of technical business requirements including planning, implementation and release",
+                        "Accomplished goals by collaborating with designers and other engineers in a fast-paced startup environment",
+                        "Designed and built iOS client application in Swift/SwiftUI, allowing patients to manage prescriptions and coordinate deliveries",
+                        "Captured larger user base by migrating iOS app functionality to cross-platform SvelteKit web application",
+                        "Provided maximum control to pharmacists through custom web applications designed to queue and automate common tasks",
+                        "Kept operating costs at a minimum by leveraging credits and free tiers offered by cloud providers such as AWS and GCP",
+                        "Reduced development and iteration cycles by migrating from the prototype Java Spring backend to a Golang and Node.js stack",
+                        "Adapted to technical requirements and limitations relating to data storage laws with creative, cost-effective solutions",
+                        "Supported self-hosted tools relating to pharmacy operations, both for our own security practices and for PIPEDA-compliance",
+                        "Implemented login methods balancing convenience and security such as magic links, one-time codes, OAuth and WebAuthn",
+                        "Improved continuity in pharmacist-patient communication by integrating our text message service with our internal chat platform",
+                        "Developed notification system to reach out to patients over text message and email",
+                        "Interfaced with Google Cloud SQL PostgreSQL instance, along with in-memory cache to reduce accesses",
                     ],
                 },
             ],
         },
         {
             company: "Bell Canada",
-            years: "2017 – 2021",
+            years: "2017–2021",
             description:
-                "After graduating I was recruited by a small and fairly new Agile team at Bell that was set to deliver on an ambitious project with a tight, government-mandated deadline. In response to the CRTC's updates to the wireless code of conduct – which all telecommunications companies must follow – my team at Bell succeeded in being the first to implement the new requirements without delay. All this was achieved by swapping out components of our legacy system with a new microservice architecture, supported by the geo-redundant infrastructure we built and managed. Later, I transferred to a team responsible for the rollout of 5G onto Bell's network, where I supported our Kubernetes environments and eliminated blockers in a high performance remote team setting.",
+                "After graduating I was recruited by a fairly new Agile team at Bell to help deliver on an ambitious project with a tight, government-mandated deadline. In response to the CRTC's updates to the wireless code of conduct – which all telecommunications companies must follow – my team made sure Bell was the first in the country to meet the new requirements without delay, and we were recognized for our quick execution. All this was achieved while we implemented new features and phased out legacy system components, replacing them with a new microservice architecture supported by the geo-redundant infrastructure my team also built and managed. I later transferred to a remote, high-performance team responsible for Bell's rollout of 5G, where I supported our adoption of and migration to Kubernetes.",
             roles: [
                 {
                     title: "Software Developer",
                     subtitle: "Infrastructure & Pipeline Automation",
                     responsibilities: [
-                        "Supported high redundancy charging systems for a nationwide telecommunications company withstanding high TPS",
-                        "Shortened testing and delivery iteration cycles by tracing blockers and performance hogs to enhance developer and QA experience",
-                        "Presented a talk to my team and to our directors to advocate for knowledge sharing and reduced redundancy by means of organization-wide “open-source” development practices",
-                        "Troubleshooted development and production environment issues, providing technical support where required, and documenting fixes and best practices in our team’s shared knowledge base",
-                        "Independently introduced, trialed and rolled out what would eventually be several key infrastructure and development tools (i.e. Terraform, Portainer)",
-
-                        "Know design patterns, implemented a bunch of features with minimal guidance",
-                        "Wrote a lot of reconciliation scripts that are probably still there today",
-                        "Supported application deployments to Openshift and their integration",
-
-                        "Minimized human error and sped up deployments by developing multi-stage pipelines which can run automated integration tests and deploy to any environment",
-                        "Gave knowledge-sharing talks and demos to my team on the topics of development environments, automation and general Linux productivity enhancements with tools such as tmux and vim",
+                        "Supported a nationwide telecommunications company's geo-redundant charging systems capable of withstanding sustained loads of upwards of 60 TPS",
+                        "Sped up testing and delivery iteration cycles by 1.5-2x and enhanced developer and QA experience by tracing blockers and performance hogs",
+                        "Gave presentations to my team of 50 and to company directors advocating for knowledge sharing and reduced redundancy by means of organization-wide “open-source” development practices",
+                        "Created and took ownership of several highly referenced team knowledge base pages documenting key deployment environment details, best practices and troubleshooting steps",
+                        "Reduce incidence of errors and rollbacks and sped up deployments by developing multi-stage pipelines which run automated integration tests and deployments",
+                        "Presented knowledge-sharing talks and demos to my team on the topics of development environments, automation and general Linux productivity enhancements with tools such as tmux and vim",
+                        "Took initiative to trial, introduce and roll out what eventually would be several key infrastructure and development tools, including but not limited to Terraform and Portainer",
+                        "Built and configured our team's first development Docker swarm, along with several staging environments",
                     ],
                 },
                 {
                     title: "DevOps Engineer",
                     subtitle: "DevOps and Java Spring Development",
                     responsibilities: [
-                        "Built and configured first development Docker swarm",
-                        "Built multiple QA and production environments (Ansible, Terraform)",
-                        "I introduced (or was instrumental in the adoption of) a number of tools to the team's stack, from which I can proudly say several still remain foundational to the team's infrastructure (i.e. Terraform, Portainer, GitLab CI).",
                         "Docker and Kubernetes (OpenShift) Ansible Terraform Jenkins, later GitLab CI Vagrant OpenStack Zabbix Graylog",
                         "Took charge in setting up automated development environment deployments ",
                         "Wrote and took ownership of deployment scripts for setting up development and production environments",
@@ -91,13 +81,18 @@
                         "Worked with DBAs to automate geo-redundant MongoDB deployments",
                         "As well during my time at Bell I wrote a script which was a key project deliverable. It provided a Terraform template and Ansible playbook to deploy a geo-redundant multi-node cluster that would support enormous streams of data. Later, I collaborated with our RDBMS specialists and Mongo's on-site support team to optimize the deployments. This consisted of dozens of test deployments, experimenting with a number of our server's OS configurations (THP, security policies, hypervisor considerations).",
                         "Developed features within an Agile-managed microservice architecture ",
+                        "Know design patterns, implemented a bunch of features with minimal guidance",
+                        "Developed a deep understanding of systems and was able to poke around to get the results we were looking for",
+                        "Wrote a lot of reconciliation scripts that are probably still there today",
+                        "Supported application deployments to Openshift and their integration",
+                        "Go-to person for handling development or deployment issues",
                     ],
                 },
             ],
         },
         {
             company: "University of Toronto",
-            years: "2016 – 2017",
+            years: "2016–2017",
             roles: [
                 {
                     title: "Teaching Assistant",
@@ -110,7 +105,7 @@
     let skills = [
         "Web development: Javascript, Typescript, HTML/CSS; building Svelte applications",
         "Scripting languages: Python, Ruby",
-        "Systems programming languages: Go, Java, Rust",
+        "Systems programming languages: Go, Java, C/C++, Rust",
         "Functional programming languages: Racket, Elm",
         "Other programming languages: Swift; building interfaces with SwiftUI",
         "Databases: PostgreSQL, PocketBase; setting up and optimizing MongoDB clusters",
@@ -120,9 +115,7 @@
         "Operating systems: comfortable using, maintaining and troubleshooting Unix-based environments (Linux, macOS)",
         "User interface design and development",
         "Creative coding, graphics programming and game engine architecture",
-        "In a personal capacity, I enjoy coding and am usually hacking away at a few side projects at any given time, which I occasionally showcase on my Github/GitLab. I like to explore new frameworks and languages and am always excited to add a new language to my tool belt.",
-        "In terms of hobbies, since I was first taught to set up and use SSH, I have been maintaining a number of home servers for the purpose of supporting self-hosted services. Although my first few servers' OS is Canonical's Ubuntu, I prefer Arch Linux for personal use. Some of my favourite self-hosted tools are MinIO and Proxmox, which I make generous use of in my personal projects.",
-        "Know my way around the command line and have set up enough Linux machines and Hackintoshes to feel comfortable",
+        "Quick to pick up new frameworks and languages",
         "Home server administration (self-hosted services, build servers, chat bots)",
         "Languages: English, Arabic, Russian",
     ];
@@ -159,7 +152,7 @@
 <section>
     <h3>Summary</h3>
     <div>
-        <p>
+        <p class="summary">
             Cloud software & infrastructure specialist with 5+ years of
             experience developing, deploying and maintaining large microservice
             systems in telecommunications and telehealth.
@@ -170,7 +163,11 @@
 <!-- TODO: Include "I am comfortable working with XYZ languages..." -->
 
 <section>
-    <h3>Experience</h3>
+    <h3>
+        Experience <h3 style="color: #00000000; position: absolute">
+            Work History
+        </h3>
+    </h3>
     <div>
         {#each experience as gig}
             <div class="company">
@@ -215,7 +212,7 @@
     <div>
         <div class="company">
             <h3>University of Toronto</h3>
-            <h3>2014 – 2019</h3>
+            <h3>2014–2019</h3>
         </div>
 
         <div class="role">
@@ -243,8 +240,9 @@
             like Red Hat's Ansible. During one of our runs, the tool misbehaved
             in a consistent way, so I opened an issue <a
                 href="https://github.com/ansible/ansible/issues/32906">[1]</a
-            >. In the meantime, I was able to pinpoint the erroneous code and
-            submit a fix that handled my use-case. Though my particular fix
+            >. In the meantime, I was able to pinpoint the code responsible for
+            the problem and submit a fix that handled my use-case. Though my
+            particular fix
             <a href="https://github.com/ansible/ansible/pull/32909">[2]</a> was not
             merged, my report was acknowledged and a similar fix was employed.
         </p>
@@ -382,6 +380,10 @@
         text-align: justify;
     }
 
+    p.summary {
+        text-align: justify;
+    }
+
     ul {
         margin: 0;
         padding-left: 1rem;
@@ -415,7 +417,7 @@
         font-size: 0.8rem;
     }
 
-    @media (max-width: 700px) {
+    @media screen and (max-width: 700px) {
         section > h3 {
             writing-mode: vertical-rl;
             text-orientation: mixed;
@@ -423,7 +425,7 @@
         }
     }
 
-    @media (max-width: 620px) {
+    @media screen and (max-width: 620px) {
         #coursework {
             column-count: 1;
         }
