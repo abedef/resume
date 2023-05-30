062a277a10d941148126a62d0312daaafb0e7d4e
Abed Fayyad <abed.ef@icloud.com>
2023-05-30 11:38:27 -0400

    Content tweaks and bold font fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -103,22 +103,62 @@
     ];
 
     let skills = [
-        "Web development: Javascript, Typescript, HTML/CSS; building Svelte applications",
-        "Scripting languages: Python, Ruby",
-        "Systems programming languages: Go, Java, C/C++, Rust",
-        "Functional programming languages: Racket, Elm",
-        "Other programming languages: Swift; building interfaces with SwiftUI",
-        "Databases: PostgreSQL, PocketBase; setting up and optimizing MongoDB clusters",
-        "Automation tools: Ansible, Jenkins, Gitlab CI/CD",
-        "Infrastructure tools: Docker, Packer, Vagrant, Terraform, HAProxy, Nginx, Traefik",
-        "Cloud providers and hypervisors: Openshift, Openstack, Google Cloud (GCP)",
-        "Operating systems: comfortable using, maintaining and troubleshooting Unix-based environments (Linux, macOS)",
-        "User interface design and development",
-        "Creative coding, graphics programming and game engine architecture",
-        "Quick to pick up new frameworks and languages",
-        "Home server administration (self-hosted services, build servers, chat bots)",
-        "Languages: English, Arabic, Russian",
+        {
+            title: "Web development:",
+            body: "Javascript, Typescript, HTML/CSS; Svelte applications",
+        },
+        { title: "Scripting languages:", body: "Python, Ruby" },
+        {
+            title: "Systems programming languages:",
+            body: "Go, Java, C/C++, Rust",
+        },
+        { title: "Functional programming languages:", body: "Racket, Elm" },
+        {
+            title: "Other programming languages:",
+            body: "Swift, SwiftUI; user interface design",
+        },
+        {
+            title: "Databases:",
+            body: "PostgreSQL, MongoDB, Supabase, PocketBase, Cloud SQL",
+        },
+        { title: "Automation tools:", body: "Ansible, Jenkins, Gitlab CI/CD" },
+        {
+            title: "Infrastructure tools:",
+            body: "Docker, Packer, Vagrant, Terraform",
+        },
+        {
+            title: "Networking tools:",
+            body: "HAProxy, Nginx, Traefik",
+        },
+        {
+            title: "Cloud providers and hypervisors:",
+            body: "Openshift, Openstack, Google Cloud (GCP)",
+        },
+        {
+            title: "Operating systems:",
+            body: "proficient in using/maintaining/troubleshooting Unix-based environments (Linux, macOS)",
+        },
+        {
+            title: "Linux server administration:",
+            body: "self-hosting CI runners, build servers, chat bots",
+        },
+        { title: "Languages:", body: "English, Arabic, Russian" },
+        // { body: "Quick to pick up new frameworks and programming languages" },
+        // { body: "Creative coding, graphics programming and game engine architecture", },
     ];
+
+    let summary =
+        "Cloud software & infrastructure specialist with 5+ years of experience developing, deploying and maintaining large microservice systems in telecommunications and telehealth. I am a self-starter with a demonstrated history of initiative and communication skills who thrives in collaborative environments. Looking for opportunities to broaden my knowledge and apply my experience while solving interesting problems.";
+
+    let links: string[] = [];
+
+    function addLink(url: string) {
+        let reference = links.length + 1;
+
+        links.push(url);
+
+        return reference;
+    }
 </script>
 
 <svelte:head>
@@ -149,130 +189,185 @@
     </div>
 </header>
 
-<section>
-    <h3>Summary</h3>
-    <div>
-        <p class="summary">
-            Cloud software & infrastructure specialist with 5+ years of
-            experience developing, deploying and maintaining large microservice
-            systems in telecommunications and telehealth.
-        </p>
-    </div>
-</section>
+<!-- FIXME: Enclosing contents with <article> tag makes reader view display each section correctly -->
 
-<section>
-    <h3>Skills</h3>
-    <div>
-        <ul>
-            {#each skills as skill}
-                <li>{skill}</li>
-            {/each}
-        </ul>
-    </div>
-</section>
-<!-- TODO: Include "I am comfortable working with XYZ languages..." -->
+<article>
+    <section>
+        <h3>Summary</h3>
+        <div>
+            <p class="summary">{summary}</p>
+        </div>
+    </section>
 
-<section>
-    <h3>
-        Experience <h3 style="color: #00000000; position: absolute">
-            Work History
-        </h3>
-    </h3>
-    <div>
-        {#each experience as gig}
-            <div class="company">
-                <h3 class="company-name">{gig.company}</h3>
-                <h3 class="years">{gig.years}</h3>
-            </div>
-            {#if "description" in gig}
-                <p class="company-description">{gig.description}</p>
-            {/if}
+    <section>
+        <h3>Skills</h3>
+        <div>
+            <ul class="skills">
+                {#each skills as skill}
+                    <li>
+                        {#if "title" in skill}
+                            <strong>{skill.title}</strong>
+                        {/if}
+                        {skill.body}
+                    </li>
+                {/each}
+            </ul>
+        </div>
+    </section>
+    <!-- TODO: Include "I am comfortable working with XYZ languages..." -->
 
-            {#each gig.roles as role}
-                <div class="role">
-                    <h4>{role.title}</h4>
-                    <em>{role.subtitle}</em>
+    <section>
+        <h3>Experience</h3>
+        <div>
+            {#each experience as gig}
+                <div class="company">
+                    <h3 class="company-name">{gig.company}</h3>
+                    <h3 class="years">{gig.years}</h3>
                 </div>
-
-                {#if "responsibilities" in role}
-                    <ul class="responsibilities">
-                        {#each role.responsibilities as responsibility}
-                            <li>{responsibility}</li>
-                        {/each}
-                    </ul>
+                {#if "description" in gig}
+                    <p class="company-description">{gig.description}</p>
                 {/if}
+
+                {#each gig.roles as role}
+                    <div class="role">
+                        <h4>{role.title}</h4>
+                        <em>{role.subtitle}</em>
+                    </div>
+
+                    {#if "responsibilities" in role}
+                        <ul class="responsibilities">
+                            {#each role.responsibilities as responsibility}
+                                <li>{responsibility}</li>
+                            {/each}
+                        </ul>
+                    {/if}
+                {/each}
             {/each}
-        {/each}
-    </div>
-</section>
-
-<section>
-    <h3>Education</h3>
-    <div>
-        <div class="company">
-            <h3>University of Toronto</h3>
-            <h3>2014–2019</h3>
         </div>
+    </section>
 
-        <div class="role">
-            <h4>Honours Bachelor of Science (HBSc) with Distinction</h4>
-            <em>Computer Science Specialist Program</em>
+    <section>
+        <h3>Education</h3>
+        <div>
+            <div class="company">
+                <h3>University of Toronto</h3>
+                <h3>2014–2019</h3>
+            </div>
+
+            <div class="role">
+                <h4>Honours Bachelor of Science (HBSc) with Distinction</h4>
+                <em>Computer Science Specialist Program</em>
+            </div>
+
+            <ul id="coursework">
+                {#each courses as course}
+                    <li>{course.code} <em>{course.name}</em></li>
+                {/each}
+            </ul>
         </div>
+    </section>
 
-        <ul id="coursework">
-            {#each courses as course}
-                <li>{course.code} <em>{course.name}</em></li>
-            {/each}
-        </ul>
-    </div>
-</section>
+    <section>
+        <h3>Open Source Projects</h3>
+        <div>
+            <div class="company">
+                <h3>Ansible</h3>
+            </div>
 
-<section>
-    <h3>Open Source Contribution</h3>
-    <div>
-        <div class="company">
-            <h3>Ansible</h3>
+            <p class="company-description">
+                During my time at Bell, our team made heavy use of automation
+                tools like Red Hat's Ansible. During one of our runs, the tool
+                misbehaved in a consistent way, so I opened an issue [{addLink(
+                    "https://github.com/ansible/ansible/issues/32906"
+                )}] outlining the problem. In the meantime, I was able to
+                pinpoint the code responsible for the problem and submit a fix
+                that handled my use-case. Though my particular fix [{addLink(
+                    "https://github.com/ansible/ansible/pull/32909"
+                )}] was not merged, my report was acknowledged and a similar fix
+                was employed.
+            </p>
+
+            <div class="company">
+                <h3>Lockwise</h3>
+            </div>
+
+            <p class="company-description">
+                I tracked down a user interface bug in Mozilla's iOS password
+                manager app, Lockwise, and submitted a fix [{addLink(
+                    "https://github.com/mozilla-lockwise/lockwise-ios/pull/1102"
+                )}] which <em>was</em> merged!
+            </p>
+
+            <div class="company">
+                <h3>boop [{addLink("https://github.com/abedef/boop-go")}]</h3>
+            </div>
+
+            <p class="company-description">
+                An experimental information capture and recall system I created
+                as an open-source alternative to a commercial product with
+                similar offerings [{addLink("https://www.tatatap.com")}].
+                Annotated text-based submissions (called <em>boop</em>s) are
+                transmitted to the system over web interface, SMS, or email (or,
+                just for fun: over DNS) and indexed, providing lightning-fast
+                information storage and retrieval.
+            </p>
+
+            <div class="company">
+                <h3>Passkey-based authentication</h3>
+            </div>
+
+            <p class="company-description">
+                I have taken a keen interest in WebAuthn – the new-ish standard
+                for logins on the web – and have implemented my own passkey
+                authentication [{addLink("https://genieindex.ca")}] in Go and
+                Javascript for various services I manage for friends and family.
+                My current goal is to bring native passkey authentication
+                support to backend frameworks such as PocketBase, which
+                currently only supports popular login methods such as email and
+                OAuth.
+            </p>
+
+            <div class="company">
+                <h3>
+                    Lie to Me – A Wikipedia Game [{addLink(
+                        "https://github.com/abedef/wikigame"
+                    )}]
+                </h3>
+            </div>
+
+            <p class="company-description">
+                Variation of a game called <em>Balderdash</em>. Players in a
+                lobby are each given a random Wikipedia article to read within a
+                time limit. Each round, an article title is randomly selected
+                from the pool of articles, and the guesser is tasked with
+                testing each player's knowledge on the topic in order to intuit
+                who had originally been assigned that article.
+            </p>
+            <p class="oss-description">
+                This project involved solving many technical challenges I had
+                not yet encountered in my line of work. For example, I use
+                WebSockets to implement real-time Jackbox-style game lobbies
+                that can be joined with a room code. I was also forced to come
+                up with creative solutions and workarounds. The random article
+                selector had initially exhibited noticable latency when pulling
+                articles from Wikipedia. After trying a few options, I was able
+                to significantly reduce the delay by implementing a
+                reverse-proxy rule on the Nginx server that hosts the game.
+            </p>
         </div>
+    </section>
 
-        <p class="company-description">
-            During my time at Bell, our team made heavy use of automation tools
-            like Red Hat's Ansible. During one of our runs, the tool misbehaved
-            in a consistent way, so I opened an issue <a
-                href="https://github.com/ansible/ansible/issues/32906">[1]</a
-            >. In the meantime, I was able to pinpoint the code responsible for
-            the problem and submit a fix that handled my use-case. Though my
-            particular fix
-            <a href="https://github.com/ansible/ansible/pull/32909">[2]</a> was not
-            merged, my report was acknowledged and a similar fix was employed.
-        </p>
-
-        <div class="company">
-            <h3>Lockwise</h3>
+    <section>
+        <h3 />
+        <div>
+            <ul class="links">
+                {#each links as link, i}
+                    <li>[{i + 1}] <a href={link}>{link}</a></li>
+                {/each}
+            </ul>
         </div>
-
-        <p class="oss-description">
-            I tracked down a user interface bug in Mozilla's iOS password
-            manager app, Lockwise, and submitted a fix <a
-                href="https://github.com/mozilla-lockwise/lockwise-ios/pull/1102"
-                >[3]</a
-            >
-            which <em>was</em> merged!
-        </p>
-    </div>
-</section>
-
-<section>
-    <h3 />
-    <div>
-        <ul class="links">
-            <li>[1] https://github.com/ansible/ansible/issues/32906</li>
-            <li>[2] https://github.com/ansible/ansible/pull/32909</li>
-            <li>
-                [3] https://github.com/mozilla-lockwise/lockwise-ios/pull/1102
-            </li>
-        </ul>
-    </div>
-</section>
+    </section>
+</article>
 
 <footer>
     <p>
@@ -329,6 +424,10 @@
     div.company {
         display: flex;
         justify-content: space-between;
+    }
+
+    ul.skills {
+        column-count: 1;
     }
 
     h3.years {
@@ -388,6 +487,13 @@
         padding-left: 1rem;
     }
 
+    ul.skills {
+        /* TODO Recieved feedback to make this a single column */
+        column-count: 2;
+        list-style: none;
+        padding: 0;
+    }
+
     ul.responsibilities {
         margin: 0;
         margin-bottom: 1rem;
@@ -407,13 +513,13 @@
 
     .role > em,
     .role > h4 {
-        font-size: 0.9rem;
+        font-size: 0.8rem;
     }
 
     p,
     ul,
     header > div {
-        font-size: 0.8rem;
+        font-size: 0.7rem;
     }
 
     @media screen and (max-width: 700px) {
@@ -422,11 +528,33 @@
             text-orientation: mixed;
             min-width: unset;
         }
+
+        ul.skills {
+            padding-left: 0;
+            list-style: none;
+        }
+
+        ul#coursework {
+            padding-left: 0;
+            list-style: none;
+        }
     }
 
     @media screen and (max-width: 620px) {
         #coursework {
             column-count: 1;
         }
+    }
+
+    @media print {
+        footer {
+            visibility: hidden;
+        }
+    }
+
+    a {
+        font-weight: bold;
+        text-decoration: none;
+        color: currentColor;
     }
 </style>
